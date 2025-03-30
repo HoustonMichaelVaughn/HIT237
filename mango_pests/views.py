59bb68557b939ec595e407e4db2306447b94d0ad
HoustonMichaelVaughn <s377726@students.cdu.edu.au>
2025-03-30 17:45:34 +1030

    Made a basic about page, did some reorganising, added static files for css, added proper links to about and home for the navbar.

--- mango_pests/views.py
+++ mango_pests/views.py
@@ -7,5 +7,14 @@ def home(request):
     return(render(request, 'mango_pests\home.html'))
 
 def about(request):
-
-    return(render(request, r'mango_pests\about.html'))
+    aboutcards = [
+        {"membername":"Houston Vaughn",
+         "aboutmember":"A computer science student at CDU. Teamleader for Group 7"},
+        {"membername":"Neolisa De Castro",
+         "aboutmember":"Temp Text"},
+        {"membername":"Gislene Freitas De Lima Clancy",
+         "aboutmember":"Temp Text"},
+        {"membername":"Dean Metcalfe",
+         "aboutmember":"Temp Text"}
+    ]
+    return(render(request, r'mango_pests\about.html',{"aboutcards":aboutcards}))
