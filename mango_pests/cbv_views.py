c2487b763870d4632fa58b0cf522c7cdb1b1eb7e
Neolisa <75787094+lisafoodie@users.noreply.github.com>
2025-04-14 16:12:48 +0930

    Update cbv_views.py

--- mango_pests/cbv_views.py
+++ mango_pests/cbv_views.py
@@ -17,13 +17,31 @@ class PestListView(TemplateView):
       #!!!!comments will be updated later!!!
  class PestDetailView(TemplateView):
   template_name = 'mango_pests/project_detail.html'
-  #create class PestListView to render the Pest List Page from template project_list.html
+  #
     def get_context_data(self,**kwargs):
-      # define method get_context_data to apply PestListView to data that is input through the template project_list.html
+      #
       context = super(). get_context_data(**kwargs)
-      #allows additional arguments to be accepted by context dictionary
+      #
       slugurl =self.kwargs['slugurl']
       for pest in Pestdiseases:
         if pest.urlslug == slugurl:
-          
+          context['pestdetails'] = pest.dictionaryconstruct()
+          break
+      return context
+      
+class AboutView(TemplateView):
+  template_name = 'mango_pests/about.html'
+  #
+    def get_context_data(self,**kwargs):
+      #
+      context = super(). get_context_data(**kwargs)
+      #
+      context['aboutcards'] = [
+        {"membername": "Houston Vaughn", "aboutmember": "A Computer Science Student"}  
+        {"membername": "Gislene Freitas de Lima Clancy", "aboutmember": "Temp Text"}
+        {"membername": "Dean Metcalfe", "aboutmember": "Temp Text"}
+        {"membername": "Neolisa de Castro", "aboutmember": "Trying to Finish Uni"}
+      ]
+      return context
+  
       
