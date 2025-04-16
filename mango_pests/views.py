f589d2153ab221d972c773ea83884813783dd39a
HoustonMichaelVaughn <s377726@students.cdu.edu.au>
2025-04-16 17:45:55 +0930

    Ahhhh fixing

--- mango_pests/views.py
+++ mango_pests/views.py
@@ -9,7 +9,7 @@ def home(request):
 
 class PestListView(View):
     def get(self, request):
-        pestcards = [pest.dictionaryconstruct() for pest in Pestsdiseases]
+        pestcards = [pest.__dict__ for pest in Pestsdiseases]
         return(render(request, 'mango_pests/project_list.html', {"pestcards": pestcards}))
 
 class PestDetailView(View):
@@ -20,43 +20,33 @@ class PestDetailView(View):
                 break
         return(render(request, 'mango_pests/project_detail.html', {"pestdetails": pestdetails}))
 
-
-def pest_detail(request, slugurl):
-    for pest in Pestsdiseases:
-        if(pest.urlslug == slugurl):
-            pestdetails = pest.dictionaryconstruct()
-            break
-    for pest in pestdetails:
-        print(pest)
-    return(render(request, 'mango_pests/project_detail.html', {"pestdetails": pestdetails}))
-
-def about(request):
-    aboutcards = [
-        {"membername":"Houston Vaughn",
-         "aboutmember":"A computer science student at CDU. Teamleader for Group 7"},
-        {"membername":"Neolisa De Castro",
-         "aboutmember":"Computer Science Student. Contacted Fellow Students to Form the Group for HIT237,  Researched and Provided NT Specific Research Materials for Mango Farm Surveillance, Involved in the Drafting of Web Application both in Conceptualization and Coding, Suggested the Inclusion of a Reference Tab to Properly Attribute Web Application Elements to Corresponding Sources
-"},
-        {"membername":"Gislene Freitas De Lima Clancy",
-         "aboutmember": " Computer Science student at CDU. Contributed pest research, data structuring, image integration, and helped shape user interaction flow for this project."},
-        {"membername":"Dean Metcalfe",
-         "aboutmember":"Temp Text"}
-    ]
-    return(render(request, r'mango_pests\about.html',{"aboutcards":aboutcards}))
+class AboutView(View):
+    def get(self, request):
+        aboutcards = [
+            {"membername":"Houston Vaughn",
+             "aboutmember":"A computer science student at CDU. Teamleader for Group 7"},
+            {"membername":"Neolisa De Castro",
+             "aboutmember":"Computer Science Student. Contacted Fellow Students to Form the Group for HIT237, Researched and Provided NT Specific Research Materials for Mango Farm Surveillance, Involved in the Drafting of Web Application both in Conceptualization and Coding, Suggested the Inclusion of a Reference Tab to Properly Attribute Web Application Elements to Corresponding Sources"},
+            {"membername":"Gislene Freitas De Lima Clancy",
+             "aboutmember":"Temp Text"},
+            {"membername":"Dean Metcalfe",
+             "aboutmember":"Temp Text"}
+        ]
+        return render(request, 'mango_pests/about.html', {"aboutcards": aboutcards})
+    
 class AboutView(View):
     def get(self, request):
         aboutcards = [
             {"membername":"Houston Vaughn",
                 "aboutmember":"A computer science student at CDU. Teamleader for Group 7"},
             {"membername":"Neolisa De Castro",
-                "aboutmember":"Computer Science Student. Contacted Fellow Students to Form the Group for HIT237,  Researched and Provided NT Specific Research Materials for Mango Farm Surveillance, Involved in the Drafting of Web Application both in Conceptualization and Coding, Suggested the Inclusion of a Reference Tab to Properly Attribute Web Application Elements to Corresponding Sources
-"},
+                "aboutmember":"Computer Science Student. Contacted Fellow Students to Form the Group for HIT237,  Researched and Provided NT Specific Research Materials for Mango Farm Surveillance, Involved in the Drafting of Web Application both in Conceptualization and Coding, Suggested the Inclusion of a Reference Tab to Properly Attribute Web Application Elements to Corresponding Sources"},
             {"membername":"Gislene Freitas De Lima Clancy",
                 "aboutmember":"Temp Text"},
             {"membername":"Dean Metcalfe",
                 "aboutmember":"Temp Text"}
             ]
         return(render(request, r'mango_pests\about.html',{"aboutcards":aboutcards}))
-class ReferencesView(TemplateView):
+#class ReferencesView(TemplateView):
     # Set the template to render as 'mango_pests/references.html'
-    template_name = 'mango_pests/references.html'
+#   template_name = 'mango_pests/references.html'
