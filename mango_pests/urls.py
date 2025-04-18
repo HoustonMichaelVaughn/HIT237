2427ae7e52307fe0ecdfbd8a40507d06d20ebfa3
HoustonMichaelVaughn <s377726@students.cdu.edu.au>
2025-04-18 13:51:29 +0930

    Formatting changes...

--- mango_pests/urls.py
+++ mango_pests/urls.py
@@ -2,9 +2,13 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^$', views.home, name="home"),
-    path('about/', views.AboutView.as_view(), name="about"),
-    path('pestlist/', views.PestListView.as_view(), name="pestlist"),
-    re_path(r'^pestlist/(?P<slugurl>[a-zA-Z-]+)/?$', views.PestDetailView.as_view(), name='pest_detail'),
-    path('references/', views.ReferencesView.as_view(), name="references"),  
+    re_path(r"^$", views.home, name="home"),
+    path("about/", views.AboutView.as_view(), name="about"),
+    path("pestlist/", views.PestListView.as_view(), name="pestlist"),
+    re_path(
+        r"^pestlist/(?P<slugurl>[a-zA-Z-]+)/?$",
+        views.PestDetailView.as_view(),
+        name="pest_detail",
+    ),
+    path("references/", views.ReferencesView.as_view(), name="references"),
 ]
--- mango_pests/views.py
+++ mango_pests/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpRequest
-from .data import Pestsdiseases,References
+from .data import Pestsdiseases, References
 from django.views import View
 import re
 
@@ -10,44 +10,68 @@ import re
 
 
 def home(request):
-    return(render(request, 'mango_pests/home.html'))
+    return render(request, "mango_pests/home.html")
+
 
 class PestListView(View):
     #   Below is a type hint to get vs to recognise request as a HttpRequest! How cool is that?
     def get(self, request: HttpRequest):
-        search = ''
+        search = ""
         try:
-            search = request.GET['search'].lower()
+            search = request.GET["search"].lower()
         except:
             pass
-        if(search):
-            pestcards = [pest.__dict__ for pest in Pestsdiseases if search in pest.cardtitle.lower()]
+        if search:
+            pestcards = [
+                pest.__dict__
+                for pest in Pestsdiseases
+                if search in pest.cardtitle.lower()
+            ]
         else:
             pestcards = [pest.__dict__ for pest in Pestsdiseases]
-        return(render(request, 'mango_pests/project_list.html', {"pestcards": pestcards, "search": search}))
+        return render(
+            request,
+            "mango_pests/project_list.html",
+            {"pestcards": pestcards, "search": search},
+        )
+
 
 class PestDetailView(View):
     def get(self, request, slugurl):
         for pest in Pestsdiseases:
-            if(pest.urlslug == slugurl):
+            if pest.urlslug == slugurl:
                 pestdetails = pest.__dict__
                 break
-        return(render(request, 'mango_pests/project_detail.html', {"pestdetails": pestdetails}))
-    
+        return render(
+            request, "mango_pests/project_detail.html", {"pestdetails": pestdetails}
+        )
+
+
 class AboutView(View):
     def get(self, request):
         aboutcards = [
-            {"membername":"Houston Vaughn",
-                "aboutmember":"A computer science student at CDU. Teamleader for Group 7"},
-            {"membername":"Neolisa De Castro",
-                "aboutmember":"Computer Science Student."},
-            {"membername":"Gislene Freitas De Lima Clancy",
-                "aboutmember":"A computer science student at CDU."},
-            {"membername":"Dean Metcalfe",
-                "aboutmember":"A computer science student at CDU."}
-            ]
-        return(render(request, 'mango_pests/about.html',{"aboutcards":aboutcards}))
-    
+            {
+                "membername": "Houston Vaughn",
+                "aboutmember": "A computer science student at CDU. Teamleader for Group 7",
+            },
+            {
+                "membername": "Neolisa De Castro",
+                "aboutmember": "Computer Science Student.",
+            },
+            {
+                "membername": "Gislene Freitas De Lima Clancy",
+                "aboutmember": "A computer science student at CDU.",
+            },
+            {
+                "membername": "Dean Metcalfe",
+                "aboutmember": "A computer science student at CDU.",
+            },
+        ]
+        return render(request, "mango_pests/about.html", {"aboutcards": aboutcards})
+
+
 class ReferencesView(View):
     def get(self, request):
-        return(render(request, 'mango_pests/references.html', {"references":References}))
+        return render(
+            request, "mango_pests/references.html", {"references": References}
+        )
