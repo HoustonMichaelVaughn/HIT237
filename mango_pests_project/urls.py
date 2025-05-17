48249fa3d7e3c1a1e75a640da1c521f4f8da1ffd
HoustonMichaelVaughn <s377726@students.cdu.edu.au>
2025-05-17 13:02:34 +0930

    Merged growers backend with Gislene UI modifications

--- mango_pests_project/urls.py
+++ mango_pests_project/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
 from mango_pests import views as v
+import growers
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mango_pests.urls')), 
+    path('', include('growers.urls'))
 ]
